79629cb36add038f1f3543e6dd46882805541b25
Pauli B <pauli.b@outlook.com>
2014-01-11 23:05:21 +0000

    publish

--- server/publish.js
+++ server/publish.js
@@ -2,15 +2,28 @@ Meteor.publish("projects", function () {
    return Projects.find({});
  });
 
-Meteor.publish("cards", function (projectId) {
-  console.log(projectId)
-   return Cards.find({projectId: projectId});
+// Meteor.publish("cards", function (projectId) {
+//   console.log(projectId)
+//    return Cards.find({projectId: projectId});
+//  });
+
+// Meteor.publish("blocks", function (projectId) {
+//    return Blocks.find({projectId: projectId});
+//  });
+
+// Meteor.publish("comments", function (projectId) {
+//    return Comments.find({projectId: projectId});
+//  });
+
+
+Meteor.publish("cards", function () {
+   return Cards.find({});
  });
 
-Meteor.publish("blocks", function (projectId) {
-   return Blocks.find({projectId: projectId});
+Meteor.publish("blocks", function () {
+   return Blocks.find({});
  });
 
-Meteor.publish("comments", function (projectId) {
-   return Comments.find({projectId: projectId});
+Meteor.publish("comments", function () {
+   return Comments.find({});
  });
