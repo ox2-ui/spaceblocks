351a68c7ef9ec291febb958982af934bf05ff3ff
Lauricio Su <me@lauricio.com>
2014-01-11 12:37:39 +0200

    initial drag implemented

---
+++ client/client.js
@@ -0,0 +1,93 @@
+Template.images.rendered = function () {
+  dragndrop();
+};
+
+var myX = '';
+var myY = '';
+var whichCard = '';
+
+UI.body.events({
+  'dragstart': function (e) {
+      Session.set("draggedItem", this._id);
+      console.log('%c dragstart   ',  'background: #FF9900; color: white; padding: 1px 15px 1px 5px;', this);
+      whichCard = e.currentTarget;
+      console.log('%c whichCard   ',  'background: #FF9900; color: white; padding: 1px 15px 1px 5px;', whichCard);
+      console.log('%c e   ',  'background: #FF9900; color: white; padding: 1px 15px 1px 5px;', e);
+      myX = e.offsetX === undefined ? e.layerX : e.offsetX;
+      myY = e.offsetY === undefined ? e.layerY : e.offsetY;
+      // resetZ();
+      whichCard.style.zIndex = 10;
+  },
+  'dragover': function (e) {
+    console.log('%c dragover   ',  'background: #FF9900; color: white; padding: 1px 15px 1px 5px;');
+
+    e.preventDefault();
+  },
+  'drop': function (e) {
+    console.log('%c drop   ',  'background: #FF9900; color: white; padding: 1px 15px 1px 5px;', this);
+    e.preventDefault();
+    console.log('%c whichCard   ',  'background: #FF9900; color: white; padding: 1px 15px 1px 5px;', whichCard);
+    // Alerts.update({_id: this._id}, {$set: {top: 1}});
+    // xcord = e;
+    // console.log('%c xcord   ',  'background: #FF9900; color: white; padding: 1px 15px 1px 5px;', xcord);
+    console.log('%c myX   ',  'background: #FF9900; color: white; padding: 1px 15px 1px 5px;', myX);
+    // whichCard.style.left = e.pageX - myX + 'px';
+    // whichCard.style.top = e.pageY - myY + 'px';
+  }
+
+});
+
+Template.cards.cards = function () {
+  return Cards.find();
+};
+
+
+// Template.cards.events({
+//   'dragstart': function (e) {
+//       // console.log('%c dragstart   ',  'background: #FF9900; color: white; padding: 1px 15px 1px 5px;', this);
+//       // whichCard = e.currentTarget;
+//       // console.log('%c whichCard   ',  'background: #FF9900; color: white; padding: 1px 15px 1px 5px;', whichCard);
+//       console.log('%c dragstart   ',  'background: #FF9900; color: white; padding: 1px 15px 1px 5px;', e);
+//       // myX = e.offsetX === undefined ? e.layerX : e.offsetX;
+//       // myY = e.offsetY === undefined ? e.layerY : e.offsetY;
+//       // resetZ();
+//       // whichCard.style.zIndex = 10;
+//   },
+//   'dragover': function (e) {
+//     console.log('%c dragover   ',  'background: #FF9900; color: white; padding: 1px 15px 1px 5px;');
+//     // console.log('%c e   ',  'background: #FF9900; color: white; padding: 1px 15px 1px 5px;', e);
+//     e.preventDefault();
+//   },
+//   'drop': function (e, t) {
+//     console.log('%c drop   ',  'background: #FF9900; color: white; padding: 1px 15px 1px 5px;', this);
+//     console.log('%c t   ',  'background: #FF9900; color: white; padding: 1px 15px 1px 5px;', t);
+//     console.log('%c e drop   ',  'background: #FF9900; color: white; padding: 1px 15px 1px 5px;', e);
+//     e.preventDefault();
+//     // console.log('%c whichCard   ',  'background: #FF9900; color: white; padding: 1px 15px 1px 5px;', whichCard);
+//     // Alerts.update({_id: this._id}, {$set: {top: 1}});
+//     // xcord = e;
+//     // console.log('%c xcord   ',  'background: #FF9900; color: white; padding: 1px 15px 1px 5px;', xcord);
+//     // console.log('%c myX   ',  'background: #FF9900; color: white; padding: 1px 15px 1px 5px;', myX);
+//     // whichCard.style.left = e.pageX - myX + 'px';
+//     // whichCard.style.top = e.pageY - myY + 'px';
+//   }
+// });
+
+Template.controls.events({
+  'click #insertCard' : function (e) {
+    console.log('%c e click   ',  'background: #FF9900; color: white; padding: 1px 15px 1px 5px;', e);
+     Cards.insert({
+       createdAt: new Date(),
+       cardName: "Test " + Random.id() ,
+       type: "",
+       left: "",
+       top: "",
+       zIndex: 0
+     }, function(err, _id){
+      if(err) {
+        console.log(err);
+      }
+    });
+  }
+});
+
---
+++ client/drag.js
@@ -0,0 +1,63 @@
+dragndrop = function() {
+  var myX = '';
+  var myY = '';
+  var whichArt = '';
+
+  function resetZ() {
+    var elements = document.querySelectorAll('img');
+    for (var i = elements.length - 1; i >= 0; i--) {
+      elements[i].style.zIndex = 5;
+    };
+  }
+
+  function moveStart(e) {
+    console.log('%c dragstart evt  ',  'background: #5D76DB; color: white; padding: 1px 15px 1px 5px;');
+    console.log('%c e start   ',  'background: #FF9900; color: white; padding: 1px 15px 1px 5px;', e);
+    whichArt = e.target;
+    myX = e.offsetX === undefined ? e.layerX : e.offsetX;
+    myY = e.offsetY === undefined ? e.layerY : e.offsetY;
+    resetZ();
+    whichArt.style.zIndex = 10;
+  }
+
+  function moveDragOver(e) {
+    console.log('%c over  evt ',  'background: #5D76DB; color: white; padding: 1px 15px 1px 5px;');
+    e.preventDefault();
+  }
+
+  function moveDrop(e) {
+    console.log('%c drop evt  ',  'background: #5D76DB; color: white; padding: 1px 15px 1px 5px;');
+    console.log('%c e drop   ',  'background: #FF9900; color: white; padding: 1px 15px 1px 5px;', this);
+    e.preventDefault();
+    var dropX = e.pageX - myX + 'px';
+    var dropY = e.pageY - myY + 'px';
+    whichArt.style.left = dropX;
+    whichArt.style.top = dropY;
+    Cards.update({_id: Session.get("draggedItem")}, { $set: { top: dropY, left: dropX, zIndex: 10 }} );
+  }
+
+// function touchStart(e) {
+//   e.preventDefault();
+//   var whichArt = e.target;
+//   var touch = e.touches[0];
+//   var moveOffsetX = whichArt.offsetLeft - touch.pageX;
+//   var moveOffsetY = whichArt.offsetTop - touch.pageY;
+//   resetZ();
+//   whichArt.style.zIndex = 10;
+
+//   whichArt.addEventListener('touchmove', function() {
+//     var positionX = touch.pageX + moveOffsetX;
+//     var positionY = touch.pageY + moveOffsetY;
+//     whichArt.style.left = positionX + 'px';
+//     whichArt.style.top = positionY + 'px';
+//   }, false);
+// } 
+
+  document.querySelector('body').addEventListener('dragstart', moveStart, false);
+  document.querySelector('body').addEventListener('dragover', moveDragOver, false);
+  document.querySelector('body').addEventListener('drop', moveDrop, false);
+
+  // document.querySelector('body').addEventListener('touchstart', touchStart, false);
+
+
+};
